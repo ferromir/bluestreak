f7e39573716e5b8457f8741119c75b2ec2fa3997
Fernando Romero <fernando.romero@road.io>
2025-12-05 09:47:06 +0100

    coverage

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,12 @@ export default {
   collectCoverageFrom: ["index.js"],
   coveragePathIgnorePatterns: ["/node_modules/"],
   testTimeout: 30000,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
